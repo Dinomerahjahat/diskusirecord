e4f815b174fd02715540bc2a19e9cfc623d77b93
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-07-03 19:31:55 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,28 +1,28 @@
-#(©)Codexbotz
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+# Rexa
 
-from pyrogram import __version__
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
 from bot import Bot
-from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from config import OWNER
+
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
-                ]
-            )
+            text=f"<b>ᴛᴇɴᴛᴀɴɢ ʙᴏᴛ ɪɴɪ :\n\n@{client.username} ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴜɴᴛᴜᴋ ᴍᴇɴʏɪᴍᴘᴀɴ ᴘᴏsᴛɪɴʜᴀɴ ᴀᴛᴀᴜ ғɪʟᴇ ʏᴀɴɢ ᴅᴀᴘᴀᴛ ᴅɪᴀᴋsᴇs ᴍᴇʟᴀʟᴜɪ ʟɪɴᴋ ᴋʜᴜsᴜs.\n\n 👨‍💼 ᴄʀᴇᴀᴛᴏʀ: @{OWNER}\n 🔗 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: <a href='https://github.com/Rexashh/onebuttonfilesharing'>File-Sharing-Man</a>\n 👨‍💻 ᴏᴡɴᴇʀ ʀᴇᴘᴏ: @JustRex\n\n👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ @tirexgugel</b>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
+            ),
         )
     elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
-        except:
+        except BaseException:
             pass
